9483718b88c28c7e7754e615092a0942df72c141
dimazvg <zvg.dmtr.it@yandex.ru>
2024-05-18 02:56:17 +0300

    p2.3. examples and Tasks

---
+++ 2. Переменные и арифметические операции/2.3. Потоки ввода-вывода. Функции putchar() и getchar()/2.3.example01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int value = getchar();
+    int res = putchar(value);
+    printf("\n%d\n", res);
+
+    return 0;
+}
---
+++ 2. Переменные и арифметические операции/2.3. Потоки ввода-вывода. Функции putchar() и getchar()/2.3.example02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int value1 = getchar();
+    int value2 = getchar();
+
+    printf("%c, %c\n", value1, value2);
+
+    return 0;
+}
