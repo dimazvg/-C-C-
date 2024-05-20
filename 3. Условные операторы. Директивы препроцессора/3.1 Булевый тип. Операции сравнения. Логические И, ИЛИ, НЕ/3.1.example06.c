1234cc87012edc17476aefd1799ff9e2e810322f
dimazvg <zvg.dmtr.it@yandex.ru>
2024-05-20 23:59:39 +0300

    p3.1 example06.c

--- 3. Условные операторы. Директивы препроцессора/3.1 Булевый тип. Операции сравнения. Логические И, ИЛИ, НЕ/3.1.example06.c
+++ 3. Условные операторы. Директивы препроцессора/3.1 Булевый тип. Операции сравнения. Логические И, ИЛИ, НЕ/3.1.example06.c
@@ -7,8 +7,11 @@ int main(void)
     int x = 15;
     //scanf("%d", &x);
     bool is_correct = (x % 2 == 0 || x % 3 == 0) && x > 5;
-
     printf("%d\n", is_correct);
+
+    bool fl_digit = x != 0 && 10 / x > 1;  // деление на 0 не произойдёт
+
+    printf("%d\n", fl_digit);
 
     return 0;
 }
