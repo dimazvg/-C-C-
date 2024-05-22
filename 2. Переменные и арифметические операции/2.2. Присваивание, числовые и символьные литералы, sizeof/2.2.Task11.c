1d0b8dc0a4a9caf87fb9724f9c91cb6069f7fe35
dimazvg <zvg.dmtr.it@yandex.ru>
2024-05-23 00:19:12 +0300

    p2.2 Tasks

---
+++ 2. Переменные и арифметические операции/2.2. Присваивание, числовые и символьные литералы, sizeof/2.2.Task11.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stddef.h>
+int main(void)
+{
+    // здесь продолжайте программу
+    double d = 0.0;
+    size_t res = sizeof(d);
+    //__ASSERT_TESTS__ // макроопределение для тестирования (не убирать и должно идти непосредственно перед return 0)
+    return 0;
+}
